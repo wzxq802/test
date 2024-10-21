90d3f1e519a0ea8e2ad41477e86b2a066dbf3307
Ulyana <ulyana20011119@gmail.com>
2024-10-21 21:31:27 +0300

    there were the function of adding notes has changed: Now you can add your own entries

--- src/components/post-add-form/post-add-form.js
+++ src/components/post-add-form/post-add-form.js
@@ -1,18 +1,48 @@
-import React from "react";
+import React, {Component} from "react";
 import './post-add-form.css';
 
-const PostAddForm = ({onAdd}) => {
-    return (
-        <div className="bottom-panel d-flex">
-            <input type="text" placeholder="Your note" className="form-control new-post-label"/>
-            <button 
-            type ="submit" 
-            className="btn btn-outline-secondary" 
-            onClick={() => onAdd('Hello')}>
-                Add
-            </button>
-        </div>
-    )
-}
+export default class PostAddForm extends Component {
 
-export default PostAddForm;
+    constructor(props) {
+        super(props);
+        this.state = {
+            text: ''
+        }
+        this.onValueChange = this.onValueChange.bind(this);
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    onValueChange(e){
+        this.setState({
+            text: e.target.value
+        })
+    }
+
+    onSubmit(e){
+        e.preventDefault();
+        this.props.onAdd(this.state.text);
+        this.setState({
+            text: ''
+        })
+    }
+
+   render() {
+    return (
+        <form 
+            className="bottom-panel d-flex"
+            onSubmit={this.onSubmit}>
+            <input 
+                type="text" 
+                placeholder="Your note" 
+                className="form-control new-post-label"
+                onChange = {this.onValueChange}
+                value={this.state.text}/>
+            <button 
+                type ="submit" 
+                className="btn btn-outline-secondary">
+                    Add
+            </button>
+        </form>
+    )
+   }
+}
